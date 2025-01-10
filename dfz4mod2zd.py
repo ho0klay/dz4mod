16e34b9d0e9386d6ba3010e47b67924749a69c7e
ho0klay <hook1aay@gmail.com>
2025-01-11 00:41:30 +0300

    Update dfz4mod2zd.py

--- dfz4mod2zd.py
+++ dfz4mod2zd.py
@@ -1,14 +1,15 @@
-a = [-3, 5, 0, -8, 1, 10]
-N = len(a)
+def insortion_sort(arr):
+	for i in range (1, len(arr)):
+		key = arr[i]
+		j = i - 1
+		while j >= 0 and key < arr[j]:
+			arr[j + 1] = arr[j]
+			j -= 1
+		arr[j + 1] = key
 
-for i in range(1, N):
-	for j in range(i, 0, -1):
-		if a[j] < a[j - 1]:
-			a[j], a[j - 1] = a[j - 1], a[j]
-		else:
-			break
-
-print(a)
+arr = [12, -8, 11, 6, -1, -5, 4]
+insortion_sort(arr)
+print(arr)
 
 
 
